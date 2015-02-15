36af4f772435123f6e40691f63fb17c2062b4309
ScottieY <scottieyan@gmail.com>
2015-02-14 23:45:25 -0500

    new

---
+++ pymo.py
@@ -0,0 +1,31 @@
+from flask import Flask
+import pymongo
+from pymongo import MongoClient
+import json
+
+
+client = MongoClient()
+
+db = client['mydb']
+
+coll = db['testData']
+
+app = Flask(__name__)
+
+@app.route("/test", methods = ['GET'])
+def show_data():
+	ls = []
+	for info in coll.find():
+		temp = info
+		#print temp.pop('_id',None)
+		ls.append(temp)
+	
+	return json.dumps(ls)
+
+@app.route("/test",methods = ['POST'])
+def get_data():
+
+	return 'post'
+
+if __name__ == "__main__":
+	app.run()
